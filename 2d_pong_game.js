ca0779f5e717e8e74f630e824c41b8b1d52146ea
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2017-08-28 21:43:27 -0500

    Initial commit

---
+++ 2d_pong_game.js
@@ -0,0 +1,46 @@
+(function(){
+
+	'use strict';
+
+	var canvas = document.getElementById('2d-pong-game-canvas');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	var ballX = canvas.width / 2;
+	var ballY = canvas.height / 2;
+	var ballRadius = 10;
+	var ctx = canvas.getContext('2d');
+	var dx = 3;
+	var dy = -3;
+
+	init();
+
+	////////////////////
+
+	function clearCanvas() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+
+	function draw() {
+		clearCanvas();
+		drawBall();
+		requestAnimationFrame(draw);
+	}
+
+	function drawBall() {
+		ctx.beginPath();
+		ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+		ctx.fillStyle = '#CCCCCC';
+		ctx.fill();
+		ctx.closePath();
+
+		// update ball coordinates
+		ballX += dx;
+		ballY += dy;
+	}
+
+	function init() {
+		draw();
+	}
+
+})();
